f97c7135ffdd11ec5411ea98757bbd4c3405296b
Balak678 <157939126+Balak678@users.noreply.github.com>
2024-04-06 18:18:42 -0400

    Update StartApplication.java

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -13,7 +13,10 @@ public class StartApplication {
     @GetMapping("/")
     public String index(final Model model) {
         model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
-            model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
+        model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
+
+        int a = 0;
+        int b = 1/a;
         return "index";
     }
 
